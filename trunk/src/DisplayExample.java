02e9a52959d0c0f65379f17a461eb69deb95c94d
nicolas.desfeux@gmail.com <nicolas.desfeux@gmail.com@47c314d6-e4b8-c27e-92b8-04ca9038210d>
2011-11-02 11:09:42 +0000

    A continuer, travailler sur le full screen !

--- trunk/src/DisplayExample.java
+++ trunk/src/DisplayExample.java
@@ -11,68 +11,132 @@ package src;
 // 05 - Can you solve the problem above? 
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.*;
-
-import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.util.vector.Vector2f;
-
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.util.glu.GLU.*;
-
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
 
 @SuppressWarnings("unused")
 public class DisplayExample {
-
-	public void start() {
+	
+	public static boolean exit=false;
+	public static boolean displayChange=false;
+    
+	public void start() throws LWJGLException {
 		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
+			DisplayMode dm = Display.getAvailableDisplayModes()[0];
+			System.out.println(dm);
+			Display.setDisplayMode(dm);
+			Display.setFullscreen(false);
+			Display.setResizable(true);
+			Display.setTitle("TP 1 - Graphics");
 			Display.create();
-			
-			
-			
-			
+
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 
 		// init OpenGL here
-		GL11.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);     // Clear The Screen And The Depth Buffer
-	    GL11.glLoadIdentity();
-	    glColor3f(0f, 1.0f, 1.0f);
-	    
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 600, 0, 100, -100);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
+		float rtri = 3f;
+        
+
+		while (!Display.isCloseRequested()) {												// Done Drawing The Quad
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);          // Clear The Screen And The Depth Buffer
+		     
+			GL11.glLoadIdentity();
+			
+			GL11.glTranslatef(100, 100, 0);
+			// set the color of the quad (R,G,B,A)
+		        GL11.glColor3f(1f,0f,0f);
+		        
+		        GL11.glPushMatrix();
+		        GL11.glRotatef(rtri, 1, 0, 0);
+		        GL11.glBegin(GL11.GL_TRIANGLES);
+					GL11.glVertex3f(0,0,0);
+					GL11.glVertex3f(100, 0, 0);
+					GL11.glVertex3f(100, 100, 0);
+				GL11.glEnd();
+				GL11.glPopMatrix();
 		
-		while (!Display.isCloseRequested()) {
-			// render OpenGL here
-			 
-			    GL11.glBegin(GL_TRIANGLES);
-				GL11.glVertex3f(0,0,0);
-				GL11.glVertex3f(1,0,0);
-				GL11.glVertex3f(1,1,0);
+				GL11.glColor3f(0f,0f,1f);
+		        //GL11.glRotatef(rtri, 0, 1, 0);
+		        GL11.glBegin(GL11.GL_TRIANGLES);
+					GL11.glVertex3f(100,0,0);
+					GL11.glVertex3f(200, 0, 0);
+					GL11.glVertex3f(200, 100, 0);
 				GL11.glEnd();
-				
-				GL11.glBegin(GL_QUADS);
-				GL11.glVertex3f(0,0,0);
-				GL11.glVertex3f(-1,0,0);
-				GL11.glVertex3f(-1,-1,0);
-				GL11.glVertex3f(0,-1,0);
-				GL11.glEnd();
-				
-			Display.update();
-			 //flushes OpenGL pipeline and swaps back and front buffers. perhaps waits for v-sync.
+				pollInput();
+			if(displayChange){
+				if(Display.isFullscreen())
+					Display.setFullscreen(false);
+				else
+					Display.setFullscreen(true);
+					
+			}
+			Display.update(); // flushes OpenGL pipeline and swaps back and
+								// front buffers. perhaps waits for v-sync.
+			rtri+=0.1f;
 		}
 
 		Display.destroy();
 	}
 
-	public static void main(String[] argv) {
+	public static void main(String[] argv) throws LWJGLException {
 		DisplayExample displayExample = new DisplayExample();
 		displayExample.start();
+	}
+	
+	
+	public void pollInput() throws LWJGLException {
+
+		if (Mouse.isButtonDown(0)) {
+			int x = Mouse.getX();
+			int y = Mouse.getY();
+
+			System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			System.out.println("SPACE KEY IS DOWN");
+		}
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Pressed");
+					displayChange=true;
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE){
+					System.out.println("Escape Key Pressed");
+					exit=true;
+				}
+			} else {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE){
+					System.out.println("Escape Key Released");
+				}
+			}
+		}
 	}
 }
